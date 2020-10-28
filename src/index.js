655f22fa6f4aabaf26e420cb9af7d5bdc57ca8f6
ludmiladev <45207186+ludmiladev@users.noreply.github.com>
2020-10-28 07:40:56 +0200

    конфиг

--- src/index.js
+++ src/index.js
@@ -1,25 +1,34 @@
-import menuTemplate from './templates/menu.hbs';
-import menuData from './menu.json';
-import './styles.css';
-
-const markup = menuTemplate(menuData);
-const menu = document.querySelector('.js-menu');
-menu.innerHTML = markup;
+import css from "./css/style.css";
+import menu from './menu.json';
+import menuTemplate from './templates/menu.hbs'
 
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
 
-const switcher = document.querySelector('.theme-switch__toggle');
+const menus = menuTemplate(menu);
 
-localStorage.getItem('checkbox') === Theme.DARK ? switcher.checked = true : switcher.checked = false
-switcher.checked ? document.body.classList.add(Theme.DARK) : document.body.classList.add(Theme.LIGHT);
+document.querySelector('.js-menu').innerHTML = menus;
 
-const changeTheme = function (event) {
-  localStorage.setItem('checkbox', event.target.checked ? Theme.DARK : Theme.LIGHT)
+
+const themeSwitch = document.querySelector('.theme-switch__toggle');
+
+if (localStorage.getItem('checkbox') === Theme.DARK) {
+  themeSwitch.checked = true;
+} else {
+  themeSwitch.checked = false
+};
+
+if (themeSwitch.checked) {
+  document.body.classList.add(Theme.DARK)
+} else {
+  document.body.classList.add(Theme.LIGHT)
+};
+ 
+
+themeSwitch.addEventListener('change', (e) => {
+  localStorage.setItem('checkbox', e.target.checked ? Theme.DARK : Theme.LIGHT)
   document.body.classList.toggle(Theme.LIGHT);
   document.body.classList.toggle(Theme.DARK);
-}
-
-switcher.addEventListener('change', changeTheme)
+})
